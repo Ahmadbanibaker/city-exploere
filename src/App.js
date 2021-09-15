7632c0494720062761486863013e154e14198605
Ahmadbanibaker <ahmadmurad502@gmail.com>
2021-09-15 21:23:06 +0300

    solved lab 6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 
- class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,13 +13,16 @@ import axios from 'axios';
       type: "",
       lat: "",
       lon: "",
-      showData: false
+      showData: false,
+      arr: [],
+      showWeather: false
     }
   }
   handleLocation = (e) => {
     let city_name = e.target.value;
     this.setState({
       city_name: city_name
+
     })
   }
   handleSubmit = (e) => {
@@ -30,34 +33,49 @@ import axios from 'axios';
     }
     axios(config).then(res => {
       let responseData = res.data[0]
+      console.log(responseData)
       this.setState({
-        city_name: responseData.display_name,
+        city_name: responseData.display_place,
         lon: responseData.lon,
         lat: responseData.lat,
         type: responseData.type,
         showData: true
 
       })
+    }).then(() => {
+
+      axios.get(`http://${process.env.REACT_APP_BK_URL}/weather?searchQuery=${this.state.city_name}&lat=${this.state.lat}&lon=${this.state.lon}`)
+        .then(res => {
+          this.setState({
+            arr: res.data,
+            showWeather: true
+          })
+          console.log(this.state.arr)
+        })
     })
   }
   render() {
     return (
       <div className="App">
-      <h1>Welcome to City explorer</h1>
-      <Formsearch handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
-      {
-          this.state.showData&&
+        <h1>Welcome to City explorer</h1>
+        <Formsearch handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
+        {
+          this.state.showData &&
           <Location city_name={this.state.city_name}
-                    type={this.state.type}
-                    lat={this.state.lat}
-                    lon={this.state.lon}
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
           />
         }
-       <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.lon}`} fluid />
-    </div>
-  );
+        <img alt='asjadj' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.lon}`} fluid />
+        {this.state.showWeather && this.state.arr.map(value => {
+          return < h2>date={value.date}
+            description={value.description}</h2>
+        }
+        )}
 
-}
+      </div>
+    )
+  }
 }
 export default App
-  
